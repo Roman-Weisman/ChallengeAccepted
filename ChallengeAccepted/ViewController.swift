9fd3f10705f1bc324ee4a3fac0168260e5dcc741
Roman Weisman <roman.weisman@gmail.com>
2019-09-03 12:44:06 -0500

    Initial Commit

---
+++ ChallengeAccepted/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChallengeAccepted
+//
+//  Created by Roman Weisman on 9/3/19.
+//  Copyright © 2019 Roman Weisman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
